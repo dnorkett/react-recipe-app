5666a71a59c59e41174a5bd49dd2c6f6c58860fc
dnorkett <dnorkett@gmail.com>
2020-04-11 00:01:54 -0500

    Updates

---
+++ precache-manifest.0d4b21e992acddaa695aa860d19f75b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90440944bf34e3fb69275c9ac89b416a",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "2c1f41ba6e28b3632b07",
+    "url": "/react-recipe-app/static/css/main.b1083df0.chunk.css"
+  },
+  {
+    "revision": "4aec4fee7d64e331c1b5",
+    "url": "/react-recipe-app/static/js/2.6e6733bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipe-app/static/js/2.6e6733bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c1f41ba6e28b3632b07",
+    "url": "/react-recipe-app/static/js/main.eba666fb.chunk.js"
+  },
+  {
+    "revision": "9c12bad10d6d7390e7f3",
+    "url": "/react-recipe-app/static/js/runtime-main.97096369.js"
+  }
+]);
